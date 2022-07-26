0c60efb0d588844855ff5e8f1fe35d92f55622ba
kardash-bp <kardasbp@gmail.com>
2022-07-26 17:53:13 +0200

    repositoriesReducers

---
+++ src/store/slices/repositoriesReducers.tsx
@@ -0,0 +1,57 @@
+import { Action, createSlice, Dispatch, PayloadAction } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+export interface StateBlueprint {
+  loading: boolean
+  error: string | null
+  data: string[]
+}
+const initialState: StateBlueprint = {
+  loading: false,
+  data: [],
+  error: null,
+}
+const repositoriesSlice = createSlice({
+  name: 'repositories',
+  initialState,
+  reducers: {
+    startSearch: () => {
+      return { loading: true, error: null, data: [] }
+    },
+    searchSuccess: (state, action: PayloadAction<string[]>): StateBlueprint => {
+      state.loading = false
+
+      state.data = action.payload
+      console.log(state)
+      return state
+    },
+    searchError: (state, action: PayloadAction<string>): StateBlueprint => {
+      state.loading = false
+      state.error = action.payload
+      console.log(state)
+      return state
+    },
+  },
+})
+export const { startSearch, searchError, searchSuccess } =
+  repositoriesSlice.actions
+export default repositoriesSlice.reducer
+
+export function getData(term: string): any {
+  return async (dispatch: Dispatch<Action>) => {
+    try {
+      const { data } = await axios.get(
+        'https://registry.npmjs.org/-/v1/search',
+        {
+          params: {
+            text: term,
+          },
+        }
+      )
+      const names = data.objects.map((d: any) => d.package.name)
+      dispatch(searchSuccess(names))
+    } catch (err: any) {
+      dispatch(searchError(err.message))
+    }
+  }
+}
